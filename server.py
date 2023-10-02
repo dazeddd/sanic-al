d0eb0e88c075f7be345668c93fec79a4910c7f55
dazeddd <dazeddd@dazedddui-MacBookAir.local>
2023-10-02 13:58:17 +0900

    Add pipfile

--- server.py
+++ server.py
@@ -1,20 +1,9 @@
 from sanic import Sanic
 from sanic.response import text
-import asyncio
-import time
 
 app = Sanic("MySanicApp")
 
-# @app.get("/")
-# async def hello_world(request):
-#     return text("Hello, world.")
+@app.get("/")
+async def hello_world(request):
+    return text("Hello, world.")
 
-@app.get("/sync")
-def sync_handler(request):
-    time.sleep(0.1)
-    return text("Done")
-
-@app.get("/async")
-async def async_handler(request):
-    await asyncio.sleep(0.1)
-    return text("Done")
